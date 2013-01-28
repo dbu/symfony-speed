ef881b9cb165d401f598f941291dd9a23cd7f0ab
David Buchmann <david.buchmann@liip.ch>
2013-01-28 19:07:21 +0100

    update to symfony 2.1 stable

--- app/autoload.php
+++ app/autoload.php
@@ -1,22 +1,8 @@
 <?php
 
-if (!$loader = include __DIR__.'/../vendor/.composer/autoload.php') {
-    $nl = PHP_SAPI === 'cli' ? PHP_EOL : '<br />';
-    echo "$nl$nl";
-    if (is_writable(dirname(__DIR__)) && $installer = @file_get_contents('http://getcomposer.org/installer')) {
-        echo 'You must set up the project dependencies.'.$nl;
-        $installerPath = dirname(__DIR__).'/install-composer.php';
-        file_put_contents($installerPath, $installer);
-        echo 'The composer installer has been downloaded in '.$installerPath.$nl;
-        die('Run the following commands in '.dirname(__DIR__).':'.$nl.$nl.
-            'php install-composer.php'.$nl.
-            'php composer.phar install'.$nl);
-    }
-    die('You must set up the project dependencies.'.$nl.
-        'Run the following commands in '.dirname(__DIR__).':'.$nl.$nl.
-        'curl -s http://getcomposer.org/installer | php'.$nl.
-        'php composer.phar install'.$nl);
-}
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+$loader = require __DIR__.'/../vendor/autoload.php';
 
 // intl
 if (!function_exists('intl_get_error_code')) {
@@ -24,3 +10,7 @@ if (!function_exists('intl_get_error_code')) {
 
     $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
 }
+
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+
+return $loader;
